c7ff24b8dd74c0cba6186fd2049544b707f6c2e7
Alina Siddiqui <alinasiddiqui1511@gmail.com>
2026-07-05 10:48:29 +0530

    Time: 1 ms (9.94%), Space: 48.1 MB (46.9%) - LeetHub

---
+++ 0050-powx-n/0050-powx-n.java
@@ -0,0 +1,17 @@
+class Solution {
+    public double solve(double x, double n){
+        if(n == 0) return 1;
+        if(n < 0){
+            return solve(1/x, -n);
+        }
+        else if(n % 2 == 0){
+            return solve(x*x, n/2);
+        }
+        else {
+            return x * solve(x*x, (n-1)/2);
+        }
+    }
+    public double myPow(double x, int n) {
+        return solve(x, (long)n);
+    }
+}
