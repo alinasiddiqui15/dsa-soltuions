eae489b98837d0105f4efcb50c62fd879ee05726
Alina Siddiqui <alinasiddiqui1511@gmail.com>
2026-07-09 18:52:32 +0530

    Time: 1 ms (81.33%), Space: 43.1 MB (17.5%) - LeetHub

---
+++ 1832-check-if-the-sentence-is-pangram/1832-check-if-the-sentence-is-pangram.java
@@ -0,0 +1,18 @@
+class Solution {
+    public boolean checkIfPangram(String sentence) {
+        if(sentence.length() < 26) return false;
+        boolean[] letters = new boolean[26];
+
+        for(char c : sentence.toCharArray()){
+            if(c >= 'a' && c <= 'z'){
+               letters[c - 'a'] = true;
+            }
+        }
+
+        //find any letter that not exist
+        for(boolean existLetter : letters){
+            if(!existLetter) return false;
+        }
+        return true;
+    }
+}
