79665c594bea0999d1dc3957e7969ad5c3dd05ee
Alina Siddiqui <alinasiddiqui1511@gmail.com>
2026-07-18 19:48:34 +0530

    Time: 5 ms (80.7%), Space: 46 MB (53.84%) - LeetHub

---
+++ 0009-palindrome-number/0009-palindrome-number.java
@@ -0,0 +1,17 @@
+class Solution {
+    public boolean isPalindrome(int x) {
+       int rev = 0;
+       int dummy = x;
+       while(x != 0){
+        int rem = x % 10;
+        rev = rev * 10 + rem;
+        x = x / 10;
+       } 
+       if(rev != dummy || rev < 0){
+        return false;
+       }
+       else{
+        return true;
+       }
+    }
+}
