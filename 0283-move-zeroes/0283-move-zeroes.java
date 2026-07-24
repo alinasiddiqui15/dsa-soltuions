7fc9ec5aadbbae42ec56c72bcd294fe601938aa7
Alina Siddiqui <alinasiddiqui1511@gmail.com>
2026-07-24 23:34:11 +0530

    Time: 1 ms (99.87%), Space: 47.3 MB (96.28%) - LeetHub

---
+++ 0283-move-zeroes/0283-move-zeroes.java
@@ -0,0 +1,18 @@
+class Solution {
+    public void moveZeroes(int[] nums) {
+        int n = nums.length;
+
+        int[] copy = new int[n];
+        int j = 0;
+        for(int i=0; i<n; i++){
+            if(nums[i] != 0){
+                copy[j] = nums[i];
+                j++;
+            }
+        }
+        //copy ke sare values ko nums me dalenge
+        for(int i=0; i<n; i++){
+            nums[i] = copy[i];
+        }
+    }
+}
