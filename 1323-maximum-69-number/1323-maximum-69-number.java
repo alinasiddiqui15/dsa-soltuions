ddbd46fe4d1f544ef7e2f93c6e7dd23b557be8d4
Alina Siddiqui <alinasiddiqui1511@gmail.com>
2026-07-29 19:46:35 +0530

    Time: 0 ms (100%), Space: 42.3 MB (26.97%) - LeetHub

--- 1323-maximum-69-number/1323-maximum-69-number.java
+++ 1323-maximum-69-number/1323-maximum-69-number.java
@@ -10,14 +10,14 @@ class Solution {
         }
         int rev1 = 0;
         int c6 = 0;
-        //check if the last first digits is 6 or not
+        //check if the last first digit is 6 or not
         while(rev > 0){ 
             int ld = rev % 10;
-            if(ld == 6 && c6 < 1){ // if it is 6 then replace it with 9
+            if(ld == 6 && c6 < 1){ // if it is 6 then replace it with 9 (only first 6 not for every 6)
                 c6++;
                 ld = 9;
             }
-            //again reverse the digits and get the answer
+            //again reverse the digit and get the answer
             rev1 = rev1 * 10 + ld;
             rev = rev / 10;
         }
