101ffdf1ed5207f2d9fe3054a6d17b6276918630
Alina Siddiqui <alinasiddiqui1511@gmail.com>
2026-07-29 19:42:59 +0530

    Time: 0 ms (100%), Space: 42.3 MB (46.27%) - LeetHub

--- 1323-maximum-69-number/1323-maximum-69-number.java
+++ 1323-maximum-69-number/1323-maximum-69-number.java
@@ -1,12 +1,23 @@
 class Solution {
     public int maximum69Number(int num) {
+        int rev = 0;
 
-        String s = String.valueOf(num);
-
-        if (s.contains("6")) {
-            s = s.replaceFirst("6", "9");
+        while(num > 0){
+            int ld = num % 10;
+            rev = rev * 10 + ld;
+            num = num / 10;
         }
-
-        return Integer.parseInt(s);
+        int rev1 = 0;
+        int c6 = 0;
+        while(rev > 0){
+            int ld = rev % 10;
+            if(ld == 6 && c6 < 1){
+                c6++;
+                ld = 9;
+            }
+            rev1 = rev1 * 10 + ld;
+            rev = rev / 10;
+        }
+        return rev1;
     }
 }
