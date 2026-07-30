0ac5cc980b308430cf1ce82118e914ca150a9a9a
Alina Siddiqui <alinasiddiqui1511@gmail.com>
2026-07-30 23:02:07 +0530

    Time: 0 ms (100%), Space: 42.9 MB (97.91%) - LeetHub

--- 0066-plus-one/0066-plus-one.java
+++ 0066-plus-one/0066-plus-one.java
@@ -1,7 +1,7 @@
 class Solution {
     public int[] plusOne(int[] digits) {
         int n = digits.length;
-
+        
         for(int i=n-1; i>=0; i--){
             if(digits[i] < 9){
                 digits[i]++;
@@ -9,7 +9,6 @@ class Solution {
             }
             digits[i] = 0;
         }
-        //below array will only excute when the array consist only 9
         int[] res = new int[n+1];
         res[0] = 1;
         return res;
