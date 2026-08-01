d4a8394ab69e0d57bd53aede768d08843307205d
Alina Siddiqui <alinasiddiqui1511@gmail.com>
2026-08-02 00:08:27 +0530

    Time: 0 ms (100%), Space: 41.9 MB (91.85%) - LeetHub

--- 1323-maximum-69-number/1323-maximum-69-number.java
+++ 1323-maximum-69-number/1323-maximum-69-number.java
@@ -1,25 +1,26 @@
 class Solution {
     public int maximum69Number (int num) {
-        int rev = 0;
-        //first we reverse the num
-        while(num > 0){
-            int rem = num % 10;
-            rev = rev * 10 + rem;
-            num = num / 10;
-        }
+       int rev = 0;
 
-        //check that after reverse if the first number is 6 then change it into 9
-        int rev1 = 0;
-        int c6 = 0; // count of 6
-        while(rev > 0){
-            int rem = rev % 10;
-            if(rem == 6 && c6 < 1){
-                c6++;
-                rem = 9;
-            }
-            rev1 = rev1 * 10 + rem;
-            rev = rev / 10;
+       //first we reverse the number
+       while(num != 0){
+        int rem = num % 10;
+        rev = rev * 10 + rem;
+        num = num / 10;
+       } 
+
+       //check if in the reverse is there any 6 number that occures first
+       int rev1 = 0;
+       int c6 = 0;
+       while(rev != 0){
+        int rem = rev % 10;
+        if(rem == 6 && c6 < 1){
+            rem = 9;
+            c6++;
         }
-        return rev1;
+        rev1 = rev1 * 10 + rem;
+        rev = rev / 10;
+       }
+       return rev1;
     }
 }
