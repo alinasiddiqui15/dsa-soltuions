a1dd6403e1d9efac3c2a4a1bf3eedc2e4b42a5b0
Alina Siddiqui <alinasiddiqui1511@gmail.com>
2026-08-10 10:18:53 +0530

    Time: 42 ms (78.29%), Space: 46.3 MB (84.05%) - LeetHub

--- 4011-count-subarrays-with-even-odd-ratio-i/4011-count-subarrays-with-even-odd-ratio-i.java
+++ 4011-count-subarrays-with-even-odd-ratio-i/4011-count-subarrays-with-even-odd-ratio-i.java
@@ -6,16 +6,11 @@ class Solution {
         for(int i=0; i<n; i++){
             int even = 0;
             int odd = 0;
+
             for(int j=i; j<n; j++){
-                if(nums[j] % 2 == 0){
-                    even ++;
-                }
-                else{
-                    odd++;
-                }
-                if(odd > 0 && even * b <= odd * a){
-                    ans++;
-                }
+                if(nums[j] % 2 == 0) even++;
+                else odd++;
+                if(odd > 0 && even * b <= odd * a) ans++;
             }
         }
         return ans;
