cdc27e8ca2dcd9b6a7d105d659d82788cb6d7223
Alina Siddiqui <alinasiddiqui1511@gmail.com>
2026-08-12 10:06:28 +0530

    Time: 0 ms (100%), Space: 41.9 MB (87.38%) - LeetHub

---
+++ 0070-climbing-stairs/0070-climbing-stairs.java
@@ -0,0 +1,14 @@
+class Solution {
+    public int climbStairs(int n) {
+        if(n == 1 || n == 2) return n;
+
+        int a = 1;
+        int b = 2;
+        for(int i = 3; i <=n; i++){
+            int ans = a + b;
+            a = b;
+            b = ans;
+        }
+        return b;
+    }
+}
