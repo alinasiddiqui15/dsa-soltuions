6055451e2890c176f781ffd89134ff2db7d581d3
Alina Siddiqui <alinasiddiqui1511@gmail.com>
2026-08-13 22:14:22 +0530

    Time: 1 ms (99.91%), Space: 46.7 MB (66.28%) - LeetHub

--- 0412-fizz-buzz/0412-fizz-buzz.java
+++ 0412-fizz-buzz/0412-fizz-buzz.java
@@ -1,22 +1,21 @@
 class Solution {
     public List<String> fizzBuzz(int n) {
-       List<String> li = new ArrayList<>();
+        List<String> ans = new ArrayList<>();
 
-      for(int i=1; i<=n; i++){
-       if(i % 3 == 0 && i % 5 == 0){
-        li.add("FizzBuzz");
-       }
-       else if(i % 3 == 0){
-        li.add("Fizz");
-       }
-       else if(i % 5 == 0){
-        li.add("Buzz");
-       }
-       else{
-        li.add(Integer.toString(i));
-       } 
-      }
-    
-    return li;
+        for(int i=1; i<=n; i++) {
+            if(i%3 ==0 && i%5==0) {
+                ans.add("FizzBuzz");
+            }
+            else if(i%3==0) {
+                ans.add("Fizz");
+            }
+            else if(i%5==0) {
+                ans.add("Buzz");
+            }
+            else {
+                ans.add(Integer.toString(i));
+            }
+        }
+        return ans;
     }
 }
