5cfc3acd733846c8471edb013d26966f3e0b89e0
Alina Siddiqui <alinasiddiqui1511@gmail.com>
2026-08-18 00:48:46 +0530

    Time: 1 ms (99.96%), Space: 94.5 MB (51.97%) - LeetHub

--- 0121-best-time-to-buy-and-sell-stock/0121-best-time-to-buy-and-sell-stock.java
+++ 0121-best-time-to-buy-and-sell-stock/0121-best-time-to-buy-and-sell-stock.java
@@ -8,7 +8,7 @@ class Solution {
             int diff = prices[i] - min;
             min = Math.min(min, prices[i]);
             max = Math.max(max, diff);
-        }
-        return max;
+        } 
+        return max;       
     }
 }
