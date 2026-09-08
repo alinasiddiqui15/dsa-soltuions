9ec52be4bddf1bded814f439ca3f0e422a8b1d9e
Alina Siddiqui <alinasiddiqui1511@gmail.com>
2026-09-08 20:29:40 +0530

    Time: 373 ms (18.55%), Space: 50.8 MB (5.07%) - LeetHub

--- 1019-next-greater-node-in-linked-list/1019-next-greater-node-in-linked-list.java
+++ 1019-next-greater-node-in-linked-list/1019-next-greater-node-in-linked-list.java
@@ -1,7 +1,8 @@
+
 class Solution {
-    public int count(ListNode head) {
+    public int count(ListNode head){
         int c = 0;
-        while(head != null) {
+        while(head != null){
             c++;
             head = head.next;
         }
@@ -14,24 +15,23 @@ class Solution {
         ListNode temp = head;
         int i = 0;
         //convert linkedlist to array
-        while(temp != null) {
+        while(temp != null){
             arr[i] = temp.val;
             temp = temp.next;
             i++;
         }
 
         int[] res = new int[n];
-
-        //find next greater element
-        for(i=0; i<n; i++) {
+        //find next greater
+        for(i=0; i<n; i++){
             res[i] = 0;
-            for(int j=i+1; j<n; j++) {
-                if(arr[j] > arr[i]) {
+            for(int j=i+1; j<n; j++){
+                if(arr[j] > arr[i]){
                     res[i] = arr[j];
                     break;
                 }
             }
-        } 
-        return res;       
+        }
+    return res;    
     }
 }
