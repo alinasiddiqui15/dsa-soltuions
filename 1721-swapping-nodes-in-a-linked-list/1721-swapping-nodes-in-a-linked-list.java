e806fd5e2a8a395a1b9f51e9fdda0879487c0655
Alina Siddiqui <alinasiddiqui1511@gmail.com>
2026-09-14 20:10:59 +0530

    Time: 5 ms (5.38%), Space: 137.3 MB (7.17%) - LeetHub

--- 1721-swapping-nodes-in-a-linked-list/1721-swapping-nodes-in-a-linked-list.java
+++ 1721-swapping-nodes-in-a-linked-list/1721-swapping-nodes-in-a-linked-list.java
@@ -1,22 +1,45 @@
-class Solution {
-    public ListNode swapNodes(ListNode head, int k) {
-        ListNode slow = head;
-        ListNode fast = head;
-
-        for(int i=0; i<k-1; i++){
-            slow = slow.next;
-        }
-        ListNode first = slow;
-
-        while(slow.next != null){
-            slow = slow.next;
-            fast = fast.next;
-        }
-        ListNode second = fast;
-
-        int temp = first.val;
-        first.val = second.val;
-        second.val = temp;
-        return head;
-    }
+class Solution { 
+    
+    public int count(ListNode head) { 
+        int c = 0; 
+        
+        while(head != null) { 
+            c++; 
+            head = head.next; 
+        } 
+        
+        return c; 
+    } 
+    
+    public ListNode swapNodes(ListNode head, int k) { 
+        
+        int len = count(head); 
+        
+        int[] arr = new int[len]; 
+        ListNode temp = head; 
+        int i = 0; 
+ 
+        // Linked List ke values array me store karna
+        while(temp != null) { 
+            arr[i] = temp.val; 
+            i++; 
+            temp = temp.next; 
+        } 
+ 
+        // kth node from beginning and kth node from end
+        int t = arr[k - 1]; 
+        arr[k - 1] = arr[len - k]; 
+        arr[len - k] = t; 
+ 
+        // Array se new Linked List banana
+        ListNode dummy = new ListNode(0); 
+        ListNode res = dummy; 
+        
+        for(int j = 0; j < len; j++) { 
+            res.next = new ListNode(arr[j]); 
+            res = res.next; 
+        } 
+        
+        return dummy.next;     
+    } 
 }
