0633813330b06e4b36aad7d4b3afaac66d6a1473
Alina Siddiqui <alinasiddiqui1511@gmail.com>
2026-09-19 22:36:57 +0530

    Time: 7 ms (43.66%), Space: 46.4 MB (26.08%) - LeetHub

--- 0647-palindromic-substrings/0647-palindromic-substrings.java
+++ 0647-palindromic-substrings/0647-palindromic-substrings.java
@@ -4,24 +4,25 @@ class Solution {
        int count = 0;
 
        for(int i=0; i<n; i++) {
+        //for odd substring
         int low = i;
         int high = i;
-        while(low >=0 && high < n && s.charAt(low) == s.charAt(high)) {
+        while(low >= 0 && high < n && s.charAt(low) == s.charAt(high)) {
             low--;
             high++;
             count++;
         }
         String curr = s.substring(low+1, high);
 
+        //for even substring
         low = i;
         high = i+1;
-        while(low >=0 && high < n && s.charAt(low) == s.charAt(high)) {
+        while(low >= 0 && high < n && s.charAt(low) == s.charAt(high)) {
             low--;
             high++;
             count++;
         }
         curr = s.substring(low+1, high);
-
        } 
        return count;
     }
